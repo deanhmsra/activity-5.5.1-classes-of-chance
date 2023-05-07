0288bb554c78bb6f4ac06e676dde3b8169535166
Hayley <hayleymdean271@gmail.com>
2023-05-07 11:16:31 -0700

    finished work

--- app.js
+++ app.js
@@ -1,28 +1,41 @@
 class Casino {
-    // Write code here
+    constructor(name, isFakeCoin){
+        this.name = name;
+        this.isFakeCoin = isFakeCoin;
+        this.timesPlayed = 0;
+    }
+
+    playGame(betAmount){
+        this.timesPlayed += 1;
+        if(Math.random() <= 0.5 || this.isFakeCoin == true){
+            console.log(this.name + 'wins!')
+        } else {
+            betAmount = betAmount * (this.timesPlayed + 1)
+            console.log('You win!' + betAmount)
+        }
+    }
 };
 
 // TESTS
-const myCasino = new Casino("HackerU Casino");
-console.log(myCasino);
-myCasino.playGame(5);
-myCasino.playGame(15);
-myCasino.playGame(25);
-myCasino.playGame(35);
+//const myCasino = new Casino("UNLV Casino");
+//console.log(myCasino);
+//myCasino.playGame(5);
+//myCasino.playGame(15);
+//myCasino.playGame(25);
+//myCasino.playGame(35);
 
 // BONUS TESTS
-/*
-const myBonusCasino = new Casino("HackerU Bonus Casino", true);
+const myBonusCasino = new Casino("UNLV Bonus Casino", true);
 console.log(myBonusCasino);
 myBonusCasino.playGame(5);
 myBonusCasino.playGame(15);
 myBonusCasino.playGame(25);
 myBonusCasino.playGame(35);
-*/
+
 
 // Extra BONUS TESTS
 /*
-const myExtraBonusCasino = new Casino("HackerU Extra Bonus Casino", false);
+const myExtraBonusCasino = new Casino("UNLV Extra Bonus Casino", false);
 console.log(myExtraBonusCasino);
 myExtraBonusCasino.rollDie(6);
 myExtraBonusCasino.rollDie(20);
